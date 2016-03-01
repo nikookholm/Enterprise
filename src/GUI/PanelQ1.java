06f82ad58dceb5883fcfee03bed6c4425294cefd
amal q <zizja4ka@yahoo.dk>
2016-03-01 12:11:38 +0100

    4 panel for DroneGUI

--- src/GUI/PanelQ1.java
+++ src/GUI/PanelQ1.java
@@ -1,7 +1,21 @@
 package GUI;
 
+import java.awt.Button;
+
 import javax.swing.JPanel;
 
 public class PanelQ1 extends JPanel {
+	
+	Button Function1, function2, function3, totalFunctin;
+	
+	public void PanelQ1() {
+		
+		
+		
+		
+		
+		
+	}
+	
 
 }
