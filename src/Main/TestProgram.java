a9d565b302ef23d2199a3419f7150b3793cc3afe
Niko <nikooftheworld@gmail.com>
2016-03-02 10:42:48 +0100

    TestProgram error free since 2016

--- src/Main/TestProgram.java
+++ src/Main/TestProgram.java
@@ -2,4 +2,16 @@ package Main;
 
 public class TestProgram extends DroneProgram {
 
+	@Override
+	public void abort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
