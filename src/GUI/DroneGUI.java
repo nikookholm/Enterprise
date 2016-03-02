1387ca60a3232a657e826621afb495fb67034656
amal q <zizja4ka@yahoo.dk>
2016-03-02 10:22:43 +0100

    Using drop-down lists

--- src/GUI/DroneGUI.java
+++ src/GUI/DroneGUI.java
@@ -21,6 +21,7 @@ public class DroneGUI {
 	
 	public DroneGUI()
 	{
+		
 		initizlaize();
 		
 	}
--- src/GUI/PanelQ1.java
+++ src/GUI/PanelQ1.java
@@ -1,32 +1,85 @@
 package GUI;
 
-import java.awt.Button;
+
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class PanelQ1 extends JPanel {
+public class PanelQ1 extends JPanel{
 	
-	Button function1, function2, function3, allInOneFunctin;
+	private JButton start, cancel;
+	private JComboBox box = new JComboBox();
+	private JTextField functions = new JTextField(5);
+	private int count = 0;
+	private String[] description = {
+		"select function", "function 1", "function 2", "function 3", " All in one"	
+	};
 	
 	public  PanelQ1() {
 		
-		setBackground(Color.BLUE);
+		 setBackground(Color.BLUE);
+		 
+			initizlaize();
 		
-		function1 = new Button("F1");
-		function2 = new Button("F2");
-		function3 = new Button("F3");
-		allInOneFunctin = new Button("F4");
+	}
+	
+	public  void initizlaize(){
 		
-		this.add(function1);
-		this.add(function2);
-		this.add(function3);
-		this.add(allInOneFunctin);
+		start = new JButton("START");
+		cancel = new JButton("CANCEL");
 		
+		for(int i=0; i<5; i++){
+			
+			box.addItem(description[count++]);
+			functions.setEditable(false);
+			start.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					if (count < description.length){
+						box.addItem(description[count++]);
+						
+					}
+				}
+			});
+			
+//			box.addActionListener(new ActionListener() {
+//				
+//				@Override
+//				public void actionPerformed(ActionEvent e) {
+//					// TODO Auto-generated method stub
+//					functions.setText("index: " + box.getSelectedIndex() + " "
+//					+ ((JComboBox) e.getSource()).getSelectedIndex());
+//					
+//				}
+//			});
+			}
 		
+		start.setEnabled(false);
 		
+		start.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+				
+				
+			}
+		});
+		cancel.setEnabled(false);
 		
-		
+		this.add(box);
+		this.add(functions);
+		this.add(start);
+		this.add(cancel);
 		
 	}
 	
