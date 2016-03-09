06c4f539d8e16ad024ef8bc6bcd61add3ccb5320
LarsPede <larspeter94@gmail.com>
2016-03-09 15:26:19 +0100

    flere røde tilføjet

--- src/Navigation/DroneNavigation.java
+++ src/Navigation/DroneNavigation.java
@@ -1,5 +1,6 @@
 package Navigation;
 
+import Common.Drone;
 import Common.POI;
 
 public class DroneNavigation implements iDroneNavigation {
@@ -7,43 +8,60 @@ public class DroneNavigation implements iDroneNavigation {
 	private static DroneSensors sensors = new DroneSensors();
 	private static DroneVision  vision  = new DroneVision();
 	
-	
+
+	/**
+	 * 
+	 */
 	@Override
 	public DroneSensors getSensors() {
 		return sensors;
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
 	public DroneVision getVision() {
 		return vision;
 	}
-
+	
+	/**
+	 * The hoverTo method sets the max height, to be sure how high it is allowed to flight.
+	 * It then flies as high as possible and hovers until another command is given.
+	 * @param height
+	 * @return void
+	 */
 	@Override
 	public void hoverTo(int height) {
-		/*
-		 * The method sets the max height, to be sure how high it is allowed to flight
-		 */
-		
+		CommandManager commands = new drone.getCommanManager();
+		commands.setMaxAltitude(height);
+		commands.getCommandManager.up();
 	}
-
+	
+	/**
+	 * 
+	 */
 	@Override
 	public void flyTo(POI interest) {
 		// TODO venter på billedebehandling
-		
+	
 	}
-
+	
+	/**
+	 * 
+	 */
 	@Override
 	public void rotateRight(int degrees) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/**
+	 * 
+	 */
 	@Override
 	public void rotateLeft(int degrees) {
 		// TODO Auto-generated method stub
-		
+	
 	}
-	
-	
-
 }
