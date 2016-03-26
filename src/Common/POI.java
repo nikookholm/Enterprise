5ace40cbd14d4392336a984a95cb073923e468a3
Niko <nikooftheworld@gmail.com>
2016-03-26 11:28:46 +0100

    Updatet POI class, så den har en construtor der tager vinkel

--- src/Common/POI.java
+++ src/Common/POI.java
@@ -6,10 +6,20 @@ public class POI {
 
 	private int x, y, z;
 	private POIType type;
+	private int angle;
 	
 	public POI(POIType type, int x, int y, int z)
 	{
-		
+		this.type = type;
+		this.x    = x;
+		this.y    = y;
+		this.z    = z;
+	}
+	
+	public POI(POIType type, int angle)
+	{
+		this.type  = type;
+		this.angle = angle;
 	}
 	
 }
