83ca935d48a425ca4d2a8b89f4b978b79a227de8
Niko <nikooftheworld@gmail.com>
2016-04-13 08:51:14 +0200

    Billede igennem forhåbentlig, mangler drone til test. HVORNÅR KOMMER DEN ?

--- src/GUI/PanelQ2.java
+++ src/GUI/PanelQ2.java
@@ -7,17 +7,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.net.URL;
 
-import javax.crypto.AEADBadTagException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-import de.yadrone.base.navdata.HDVideoStreamData;
-import de.yadrone.base.navdata.VideoListener;
-import de.yadrone.base.navdata.VideoStreamData;
 import de.yadrone.base.video.ImageListener;
 
 public class PanelQ2 extends JPanel{
@@ -96,11 +92,13 @@ public class PanelQ2 extends JPanel{
 		public void updateCameraPanel(Image image)
 		{
 			System.out.println("UPDATING!");
-			System.out.println("Image dimentions: (" + image.getWidth() + ", " + image.getHeight() + ")");
-			this.image = image;
-			paint(this.getGraphics());
-		}
-		
+			System.out.println("Image is here, go to form pleeeease");
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run()
+				{
+					 cameraPanel.repaint();
+				}
+			});
 		
 	}
 	public class VideoVeiw implements ImageListener{
@@ -112,6 +110,6 @@ public class PanelQ2 extends JPanel{
 		}
 
 	}
-	
 		
+}
 }
