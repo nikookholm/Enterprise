954bf3e7199c73069f43034762dea7fe7a5aeb49
Thomas <thomas_elbo@live.dk>
2016-04-20 09:44:53 +0200

    VectorDistance, yo!

--- src/Navigation/vectorDistance.java
+++ src/Navigation/vectorDistance.java
@@ -7,10 +7,42 @@ import Vector.Vector3D;
 
 public class vectorDistance {
 	
-	public void calcCoordinates(Vector3D object1, Vector3D object2){
-		double alpha = Math.acos(object1.getXCoord()*object2.getXCoord()+object1.getXCoord()*object2.getYCoord()/(Math.sqrt(Math.pow(object1.getYCoord(), 2)+(Math.pow(object1.getYCoord(), 2))*Math.sqrt(Math.pow(object2.getXCoord(), 2)+(Math.pow(object2.getYCoord(), 2))))));
-		System.out.print(alpha);
-	
+	public void calcCoordinates(Vector3D object1, Vector3D object2, Vector3D object3) {
+		double dotAB = dot(object1, object2);
+		double dotBC = dot(object2, object3);
+		double magnitudeA = magnitude(object1);
+		double magnitudeB = magnitude(object2);
+		double magnitudeC = magnitude(object3);
+
+		double alpha;
+		double beta;
+
+		alpha = Math.acos(dotAB / (Math.abs(magnitudeA) * Math.abs(magnitudeB)));
+		beta = Math.acos(dotBC / Math.abs(magnitudeB) * Math.abs(magnitudeC));
+		System.out.print(radToDegree(alpha));
+		
+		
+
+	}
+
+	double dot(Vector3D object1, Vector3D object2) {
+		double sum;
+
+		sum = object1.getXCoord() * object2.getXCoord() + object1.getYCoord() * object2.getYCoord();
+
+		return sum;
+	}
+
+	double magnitude(Vector3D object) {
+		double sum;
+
+		sum = Math.sqrt(Math.pow(object.getXCoord(), 2) + Math.pow(object.getYCoord(), 2));
+
+		return sum;
+	}
+
+	double radToDegree(double value){
+		return (value*(180)/Math.PI);
 	}
 	
 }
