e6e116454c719c34b739c87939fc4c58b29e270e
Niko <nikooftheworld@gmail.com>
2016-04-27 10:06:35 +0200

    Udkast til droneprogram der tester rotation

---
+++ src/DronePrograms/RotationTestProgram.java
@@ -0,0 +1,42 @@
+package DronePrograms;
+
+import Common.Drone;
+import Main.DroneProgram;
+
+public class RotationTestProgram extends DroneProgram {
+
+	@Override
+	public void abort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getProgramName() {
+		return "Spinn'";
+	}
+
+	@Override
+	public void run() {
+		
+		drone.setMaxAltitude(1);
+		drone.takeOf();
+		
+		System.out.println("Vinkel fra start: " + drone.getAngle());
+		
+		for (int i = 1; i <= 4; i++)
+		{
+			drone.getMovement().rotateAngle(90);
+			System.out.println("Drejet 90 grader! Skulle have drejet (" + i*90 + " grader)");
+			Thread.sleep(1000);
+		}
+		
+		System.out.println("Vinkel til slut: " + drone.getAngle());
+		
+		drone.landing();
+		
+	}
+
+	
+	
+}
