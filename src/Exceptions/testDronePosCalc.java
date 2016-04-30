eb22f1c506a6399e22ef63beba1f6c6e7ed1bc50
Elbo <thomas.m.elbo@gmail.com>
2016-04-30 20:03:23 +0200

    Continued work on vector distance

--- src/Exceptions/testDronePosCalc.java
+++ src/Exceptions/testDronePosCalc.java
@@ -8,15 +8,15 @@ public class testDronePosCalc {
 	public static void main(String[] args) {
 		vectorDistance VD = new vectorDistance();
 		
-		Vector3D wallmark1 = new Vector3D(9,9,0);
-		Vector3D wallmark2 = new Vector3D(11,7,0);
-		Vector3D wallmark3 = new Vector3D(10,5, 0);
+		Vector3D wallmark1 = new Vector3D(0.0,0.0,0);
+		Vector3D wallmark2 = new Vector3D(1.0,2.0,0);
+		Vector3D wallmark3 = new Vector3D(0.0,4.0, 0);
 		double a = 2.83;
 		double b = 2.24;
-		double alpha = 18.43;
-		double beta = 15.95;
-		Vector3D dronePos = VD.calcDronePos(wallmark1, wallmark2, wallmark3, a, b, alpha, beta);
-		System.out.println(dronePos.getXCoord()+ ","+ dronePos.getYCoord());
+		double alpha = 0.18;
+		double beta = 0.20;
+		Vector3D dronePos = VD.calcDronePos(wallmark1, wallmark2, wallmark3, alpha, beta);
+	//	System.out.println(dronePos.getXCoord()+ ","+ dronePos.getYCoord());
 	}
 
 }
--- src/Navigation/vectorDistance.java
+++ src/Navigation/vectorDistance.java
@@ -1,5 +1,6 @@
 package Navigation;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import POI.*;
@@ -27,67 +28,76 @@ public class vectorDistance {
 	 * Method returns Vector -1,-1,-1 if it was unable to find Drone position.
 	 */
 
-	public Vector3D calcDronePos(Vector3D wallmark1, Vector3D wallmark2, Vector3D wallmark3, double a, double b,
+	public Vector3D calcDronePos(Vector3D wallmark1, Vector3D wallmark2, Vector3D wallmark3,
 			double alpha, double beta) {
 		System.out.println("WM1: " + wallmark1.getXCoord() + "," + wallmark1.getYCoord() + " WM2: " + wallmark2.getXCoord() + "," + wallmark2.getYCoord() + " WM3: " + wallmark3.getXCoord() + "," + wallmark3.getYCoord() + " Alpha: " + alpha + " Beta: "+ beta);
-		alpha = degreeToRad(alpha);
-		beta = degreeToRad(beta);
-		
-		System.out.println("Angles after conversion, alpha: " + alpha + " beta: " + beta);
-		double c1_r = (0.5*a / (Math.sin(alpha)));
-		Vector3D c1_c = new Vector3D(0, 0, 0);
-		
-		double l1 = -wallmark2.getXCoord() + wallmark1.getXCoord();
-		double l2 = magnitude(wallmark1,wallmark2);
-		double l3 = Math.sqrt(((Math.pow(a, 2)))/(Math.pow(Math.sin(alpha), 2)));
-		double l5 = (1)/(2*wallmark1.getYCoord());
-		double l6 = (1)/(2*wallmark2.getYCoord());
-		
-		c1_c.incYCoord(0.5 * (l1/l2)*(l3)+l5+l6);
-//		c1_c.incXCoord(0.5 * (wallmark2.getXCoord() - wallmark1.getXCoord() / magnitude(wallmark1, wallmark2))
-//				* Math.sqrt(Math.pow(a, 2) / Math.pow(Math.sin(alpha), 2)) + (1 / (2 * wallmark1.getYCoord()))
-//				+ 1 / (2 * wallmark2.getYCoord()));
-		
-		double c2_r = (0.5*b / (Math.sin(beta)));
-		System.out.println("c1_r: " + c1_r +" c2_r: "+c2_r);
-		System.out.println("l1: " + l1);
-		System.out.println("l2: " + l2);
-		System.out.println("l3: " + l3);
-		System.out.println("l5: " + l5);
-		System.out.println("l6: " + l6);
-		Vector3D c2_c = new Vector3D(0, 0, 0);
 
+
+		
+
+		double c1_r = (0.5*magnitude(wallmark1, wallmark2) / (Math.sin(alpha)));
+		double c2_r = (0.5*magnitude(wallmark2, wallmark3) / (Math.sin(beta)));
+		
+		Vector3D c1_c = calcCircleCoords(wallmark1, wallmark2, alpha);
+		Vector3D c2_c = calcCircleCoords(wallmark2, wallmark3, beta);
+
+		System.out.println("C1: " + c1_c.getXCoord() + ", " + c1_c.getYCoord() + " radius: " + c1_r);
+		System.out.println("C2: " + c2_c.getXCoord() + ", " + c2_c.getYCoord() + " radius: " + c2_r);
+			
 		ArrayList<Vector3D> p1_pot = new ArrayList<>();
 		ArrayList<Vector3D> p2_pot = new ArrayList<>();
-
-		for (double x = 0; x < 10.78; x += 0.01) {
-			for (double y = 0; y < 9.63; y += 0.01) {
-			//	System.out.println("Result need to be: " + c1_r + " is actually: " + (Math.pow(x, 2)-c1_c.getXCoord()*x + Math.pow(c1_c.getXCoord(), 2)+Math.pow(y, 2)-c1_c.getYCoord()*y + Math.pow(c1_c.getYCoord(), 2)) + " x: " + x + " y: " + y);
-				if ((Math.pow(x, 2)-c1_c.getXCoord()*x + Math.pow(c1_c.getXCoord(), 2)+Math.pow(y, 2)-c1_c.getYCoord()*y + Math.pow(c1_c.getYCoord(), 2)) == c1_r) {
-					System.out.println("success!");
-					p1_pot.add(new Vector3D(x, y, 0));
-				}
-				if (Math.pow(x, 2) - c2_c.getXCoord() + Math.pow(c2_c.getXCoord(), 2) + Math.pow(y, 2)
-						- c2_c.getYCoord() * y + Math.pow(c2_c.getYCoord(), 2) == c2_r) {
-					p2_pot.add(new Vector3D(x, y, 0));
+		
+		System.out.println(Math.pow(1.0 - c1_c.getXCoord(), 2)+Math.pow(2.0 - c1_c.getYCoord(), 2));
+		c1_r = Double.parseDouble(new DecimalFormat("#,##").format(c1_r));
+		for(double x = 0; x < 10; x+=0.1){
+			for(double y = 0; y < 10; y+=0.1){
+				if(allowedRange(x,y, c1_c, c2_c)){
+					System.out.println("Success at: " + x + ", " + y);
 				}
 			}
 		}
-		System.out.println((1 / (2 * wallmark2.getXCoord())));
-		System.out.println(p1_pot.size());
-		System.out.println(p2_pot.size());
-		for (int i = 0; i < p1_pot.size(); i++) {
-			for (int j = 0; j < p2_pot.size(); j++) {
-				if (p1_pot.get(i).getXCoord() == p2_pot.get(j).getXCoord()
-						&& p1_pot.get(i).getYCoord() == p2_pot.get(j).getYCoord()) {
-					if (p1_pot.get(i).getXCoord() < 0 | p1_pot.get(i).getYCoord() < 0) {
-						return p1_pot.get(i);
-					}
-				}
-			}
-		}
-
+		
 		return new Vector3D(-1, -1, -1);
+	}
+	
+	Vector3D calcCircleCoords(Vector3D wallmark1, Vector3D wallmark2, double alpha){
+		Vector3D circle = new Vector3D(0,0,0);
+
+		double a = magnitude(wallmark1, wallmark2);
+	
+		double t1 = a*a;
+
+		double t2 = Math.sin(alpha);
+
+		
+		
+		double t3 = t2*t2;
+		double t7 = Math.sqrt((1.0 / t3 * t1 - t1));
+	
+		double t8 = wallmark1.getYCoord() - wallmark2.getYCoord();
+	
+		double t10 = t8*t8;
+		
+		double t12 = wallmark1.getXCoord() - wallmark2.getXCoord();
+		double t13 = t12*t12;
+		
+		double t15 = Math.sqrt((t10+t13));
+		
+		
+		double c_x = -t8 / t15 * t7 / 2.0 +wallmark1.getXCoord() / 2.0 + wallmark2.getXCoord() / 2.0;
+		
+		double c_y = t12 / t15 * t7 / 2.0 + wallmark1.getYCoord() / 2.0 + wallmark2.getYCoord() / 2.0;
+	
+		
+		
+		circle.incXCoord(-t8 / t15 * t7 / 2.0 + wallmark1.getXCoord() / 2.0 + wallmark2.getXCoord() / 2.0);
+		circle.incYCoord(t12 / t15 * t7 / 2.0 + wallmark1.getYCoord() / 2.0 + wallmark2.getYCoord() / 2.0);
+	
+		double r = 0.5*a/Math.sin(alpha);
+	
+		circle.incZCoord(r);
+		
+		return circle;
 	}
 
 	double dot(Vector3D object1, Vector3D object2) {
@@ -109,17 +119,18 @@ public class vectorDistance {
 	double magnitude(Vector3D object1, Vector3D object2) {
 		double sum;
 
-		sum = Math.sqrt(Math.pow(Math.abs(-object2.getYCoord() + object1.getYCoord()), 2)
-				+ Math.pow(Math.abs(-object2.getXCoord() + object1.getXCoord()), 2));
+		double xt = Math.pow(object1.getXCoord()-object2.getXCoord(), 2);
+		double yt = Math.pow(object1.getYCoord()-object2.getYCoord(), 2);
+		sum = Math.sqrt(xt + yt);
 
 		return sum;
 	}
 
 	double radToDegree(double value) {
-		return (value * (180) / Math.PI);
+		return (value * ((double)180/Math.PI));
 	}
 	
 	double degreeToRad(double value){
-		return ((value * Math.PI)/180);
+		return (value * (Math.PI/(double)180));
 	}
 }
