f05c1cd3e449642509e83387f2e4c432232dddf4
Niko <nikooftheworld@gmail.com>
2016-05-04 07:52:47 +0200

    Opdatering til DroneMovement interfacet

--- src/Movements/iDroneMovement.java
+++ src/Movements/iDroneMovement.java
@@ -4,9 +4,9 @@ import POI.POI;
 
 public interface iDroneMovement {
 	
-	public void hoverTo(int height);   // Fly to the height and stay there.
-	public void   flyTo(POI interest); // Align it self to the POI.
-	public void rotateToAngle(int angle);
+	public void hoverTo(int height);      // Fly to the height and stay there.
+	public void flyTo(POI interest);      // Align it self to the POI.
+	public void rotateToAngle(int angle); 
 	public void flyForward();
 	public void flyForward(int cm);
 	public void flyHome();
