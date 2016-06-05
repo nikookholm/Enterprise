7938383852f7b51bd324296f0b315d1c7a52e909
AnwarC <AnwarC@AnwarChoukair.lan>
2016-06-05 13:44:33 +0200

    sat koordinaterne til auto for wallllls.

--- src/Navigation/QRPoi.java
+++ src/Navigation/QRPoi.java
@@ -16,6 +16,7 @@ public class QRPoi {
 	private BufferedImage QRimg;
 	private Point centrum;
 	private double distance;
+	private Point cords;
 
 	public void setX(double k){
 		xpos = k;
@@ -26,6 +27,9 @@ public class QRPoi {
 	
 	public void setCode(String s){
 		message = s;
+		if(message.startsWith("W")){
+			setCords();
+		}
 	}
 	
 	public String getCode(){
@@ -66,6 +70,9 @@ public class QRPoi {
 	public Point getLP(){
 		return lastPoint;
 	}
+	public Point getCords() {
+		return cords;
+	}
 	
 	public QRPoi(int top1, int mid1 , int bot1){
 		this.top = top1;
@@ -90,6 +97,98 @@ public class QRPoi {
 	}
 	public double getDistance() {
 		return distance;
+	}
+	
+	private void setCords(){
+		switch(this.message){
+		
+		case "W00.00":
+			this.cords = new Point(188,1055);
+			break;
+		case "W00.01":
+			this.cords = new Point(338,1060);
+			
+			break;
+		case "W00.02":
+			this.cords = new Point(515,1055);
+			
+			break;
+		case "W00.03":
+			this.cords = new Point(694,1060);
+			
+			break;
+		case "W00.04":
+			this.cords = new Point(840,1055);
+			
+			break;
+		case "W01.00":
+			this.cords = new Point(926,904);
+			
+			break;
+		case "W01.01":
+			this.cords = new Point(926,721);
+			
+			break;
+		case "W01.02":
+			this.cords = new Point(926,566);
+			
+			break;
+		case "W01.03":
+			this.cords = new Point(926,324);
+			
+			break;
+		case "W01.04":
+			this.cords = new Point(926,115);
+			
+			break;
+		case "W02.00":
+			this.cords = new Point(847,-10);
+			
+			break;
+		case "W02.01":
+			this.cords = new Point(656,-77);
+			
+			break;
+		case "W02.02":
+			this.cords = new Point(420,0);
+			
+			break;
+		case "W02.03":
+			this.cords = new Point(350,0);
+			
+			break;
+		case "W02.04":
+			this.cords = new Point(150,0);
+			
+			break;
+		case "W03.00":
+			this.cords = new Point(0,108);
+			
+			break;
+		case "W03.01":
+			this.cords = new Point(0,357);
+			
+			break;
+		case "W03.02":
+			this.cords = new Point(0,561);
+			
+			break;
+		case "W03.03":
+			this.cords = new Point(0,740);
+			
+			break;
+		case "W03.04":
+			this.cords = new Point(0,997);
+			
+			break;
+			
+			default:
+				break;
+			
+			
+		
+		
+		}
 	}
 	
 	
