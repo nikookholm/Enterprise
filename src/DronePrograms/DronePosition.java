26d29645fee66cd86e6c2cc467e79ac2b9ea0711
onepiece <favad_saidi13@hotmail.com>
2016-06-20 12:23:17 +0200

    Tilføjet test af initialSearch i dronePosition i DronePorgrams

--- src/DronePrograms/DronePosition.java
+++ src/DronePrograms/DronePosition.java
@@ -1,8 +1,12 @@
 package DronePrograms;
 
+import java.util.ArrayList;
+
 import Common.Drone;
 import Main.DroneProgram;
+import Main.Enterprise;
 import Navigation.DroneVision;
+import POI.POI;
 import Vector.Vector3D;
 import de.yadrone.apps.controlcenter.plugins.keyboard.KeyboardCommandManager;
 
@@ -21,17 +25,20 @@ public class DronePosition extends DroneProgram {
 	@Override
 	public void run() {
 		
-		Drone d = getDrone();
-		KeyboardCommandManager keys = new KeyboardCommandManager(d);
-		d.getCommandManager().setMinAltitude(1450);
-		DroneVision v = d.getNavigation().getVision();
-		
-		d.getMovement().start();
-		d.getMovement().hoverTo(1500);
-		d.getMovement().spinRight();
-		Vector3D dronePos = v.dronePosition(true);
-		System.out.println(dronePos.getXCoord() +"<<<---- X . Y---->>>"+ dronePos.getYCoord() + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-		d.getMovement().landing();
+		DroneVision v = new DroneVision(getDrone());
+		v.initialSearch(new ArrayList<POI>());
+	
+//		Drone d = getDrone();
+//		KeyboardCommandManager keys = new KeyboardCommandManager(d);
+//		d.getCommandManager().setMinAltitude(1450);
+//		DroneVision v = d.getNavigation().getVision();
+//		
+//		d.getMovement().start();
+//		d.getMovement().hoverTo(1500);
+//		d.getMovement().spinRight();
+//		Vector3D dronePos = v.dronePosition(true);
+//		System.out.println(dronePos.getXCoord() +"<<<---- X . Y---->>>"+ dronePos.getYCoord() + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+//		d.getMovement().landing();
 	}
 	
 }
